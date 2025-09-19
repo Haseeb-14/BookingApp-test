fc643ff496b150008a4b8f54f65cab696e36a502
Haseeb-14 <haseeb.sidd0@gmail.com>
2025-09-19 16:06:17 +0500

    profile screen

--- src/navigators/auth-navigator.tsx
+++ src/navigators/auth-navigator.tsx
@@ -3,12 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {
   WelcomeScreen,
   LoginScreen,
-  CreateAccountScreen,
-  CreatePasswordScreen,
-  LearnMoreScreen,
-  PrivacyPolicyScreen,
-  ConnectPillBoxScreen,
-  PairDeviceScreen,
+ 
 } from '@screens';
 import { AuthNavigationParamList, ERoutes } from '@types';
 import { OnboardingCompleteScreen } from '@screens/onboarding-complete';
@@ -23,27 +18,7 @@ export const AuthNavigator = () => {
     >
       <AuthStack.Screen name={ERoutes.WELCOME} component={WelcomeScreen} />
       <AuthStack.Screen name={ERoutes.LOGIN} component={LoginScreen} />
-      <AuthStack.Screen
-        name={ERoutes.CREATE_ACCOUNT}
-        component={CreateAccountScreen}
-      />
-      <AuthStack.Screen
-        name={ERoutes.CREATE_PASSWORD}
-        component={CreatePasswordScreen}
-      />
-      <AuthStack.Screen name={ERoutes.LEARN_MORE} component={LearnMoreScreen} />
-      <AuthStack.Screen
-        name={ERoutes.PRIVACY_POLICY}
-        component={PrivacyPolicyScreen}
-      />
-      <AuthStack.Screen
-        name={ERoutes.CONNECT_PILLBOX}
-        component={ConnectPillBoxScreen}
-      />
-      <AuthStack.Screen
-        name={ERoutes.PAIR_DEVICE}
-        component={PairDeviceScreen}
-      />
+     
       <AuthStack.Screen
         name={ERoutes.ONBOARDING_COMPLETE}
         component={OnboardingCompleteScreen}
--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ProfileScreen = () => {
+export const ProfileScreen = () => {
   return (
     <View>
       <Text>profile</Text>
     </View>
   )
 }
-
-export default ProfileScreen
 
 const styles = StyleSheet.create({})
